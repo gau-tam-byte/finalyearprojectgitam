77d7b158f2672913bc7be6a780ce615a12e52f56
gau-tam-byte <gautamkumark1214@gmail.com>
2023-03-16 13:14:19 +0530

    backendcode

---
+++ db/dbconnection.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+
+// const mongoose = require('mongoose')
+
+
+const db = 'mongodb://127.0.0.1:27017/?directConnection=true';
+mongoose.connect(db).then(()=>{
+  console.log("connection success")
+}).catch((err) =>{
+  console.log(console.error(err),"connection not sucess")
+})
+
+
+
+// const tt = function (){
+
+//   const connect = mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true')
+
+// if(!connect){
+//   console.log("Connection failed")
+// }else{
+//   console.log("connection sucessful")
+// }
+// }
+
+// module.exports = tt;
